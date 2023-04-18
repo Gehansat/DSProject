538ea870de5640cc4b41fe61b1a1eb0f03926d30
IT21063664 <it21063664@my.sliit.lk>
2023-04-18 13:30:54 +0530

    Niduki first commit

--- DSProject/BACKEND/server.js
+++ DSProject/BACKEND/server.js
@@ -1,40 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const dotenv = require("dotenv");
-require("dotenv").config();
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const http = require('http');
+require('./connection');
+const server = http.createServer(app);
+const{Server} = require('socket.io');
+const io = new Server(server, {
+    cors:'*',
+    methods:'*'
+})
 
-//App middleware
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/images', imageRoutes);
 
 
-//Create Database connection
 
-const PORT = process.env.PORT || 8070;
-const URL = process.env.DB_URL;
-mongoose.connect(URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+server.listen(8080, () => {
+    console.log('server running ay port' , 8080)
 })
-
-const connection = mongoose.connection;
-connection.once("open",() => {
-    console.log("Mongodb connection success");
-})
-
-//define Item model and connect to Item table
-const ItemRouter = require('./routes/StockItems');
-const StudentRouter = require('./routes/studs');
-
-
-app.use('/StockItems', ItemRouter);
-app.use('/studs',StudentRouter);
-
-app.listen(PORT, ()=>{
-    console.log(`App is running on ${PORT}`);
-});
